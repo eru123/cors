b6634ffd32a3300180c42607e34b3487f39be9de
eru123 <yeoligoakino@gmail.com>
2022-09-22 04:26:45 -0400

    Change limit to 60/min/ip

--- src/handlers/limiter.js
+++ src/handlers/limiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1hr
-    max: 200, // 200 requests per hour
+    windowMs: 60000, // 1 Minute
+    max: 60, // limit each IP to 60 requests per 1 minute
     standardHeaders: true,
     legacyHeaders: false,
 })
